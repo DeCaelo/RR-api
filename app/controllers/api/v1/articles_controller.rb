2a8dc0626580e5095bbdb7155e9858b12ff98275
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-16 15:17:12 +0100

    set create & destroy action in articles controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -8,12 +8,20 @@ module Api
       end
 
       def create
+        @article= Post.create(article_params)
+        if @article.save
+          render json: @article, status: 201
+        else
+          render json: { errors: @article.errors.full_messages }, status: 422
+        end
       end
 
       def show
       end
 
       def destroy
+        @article.destroy
+        render :show, status: :ok
       end
 
       private
